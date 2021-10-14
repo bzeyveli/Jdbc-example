4e71a43d99c8517a75d527d185eb642643f225ea
zeyveli <zeyveli@DESKTOP-11TMN9T>
2021-10-14 13:11:21 +0300

    Runner düzenlendi.

--- 01-jdbc/src/com/example/jdbc/runner/Runner.java
+++ 01-jdbc/src/com/example/jdbc/runner/Runner.java
@@ -4,50 +4,45 @@ import java.sql.SQLException;
 
 import com.example.jdbc.business.UserManager;
 import com.example.jdbc.business.UserManagerHelper;
-import com.example.jdbc.dao.abstracts.UserDaoManagerHelper;
 import com.example.jdbc.dao.concreate.UserDaoManager;
 import com.example.jdbc.entitiy.Address;
 import com.example.jdbc.entitiy.Company;
 import com.example.jdbc.entitiy.Geo;
 import com.example.jdbc.entitiy.User;
-import com.example.jdbc.utils.JdbcUtils;
 
 public class Runner {
 	public static void main(String[] args) {
-		
+
 		User user = new User();
 		user.setName("bekir");
 		user.setEmail("bekir zeyveli");
 		user.setPhone("111111111111");
 		user.setWebsite("zeyveli");
-		
-		
-		Address address =new Address();
+
+		Address address = new Address();
 		address.setStreet("malatya");
 		address.setCity("istanbul");
 		address.setZipcode("10000");
-		
-		Geo geo =new Geo();
+
+		Geo geo = new Geo();
 		geo.setLat(1200000);
 		geo.setLgn(102220);
 		address.setGeo(geo);
-		
+
 		Company company = new Company();
 		company.setName("zeyveli");
 		company.setCatchPhrase("malatya");
 		company.setBs("istanbul");
-		
+
 		user.setAddress(address);
 		user.setCompany(company);
-	
-		UserManagerHelper userManagerHelper  = new UserManager(new UserDaoManager());
+
+		UserManagerHelper userManagerHelper = new UserManager(new UserDaoManager());
 		try {
 			userManagerHelper.create(user);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
 }
